a4804a80c98192e6f10a4233c70bbc3d42361a7c
qinweizhaozz <35877229+qinweizhaozz@users.noreply.github.com>
2018-01-29 11:37:36 -0500

    Update test_class.py

--- test_class.py
+++ test_class.py
@@ -4,7 +4,7 @@ class TestClass(object):
     def test_one(self):
         x = 2
         y = 8
-        assert x / float(8) == 0.25
+        assert x / y == 0.25
 
     def test_two(self):
         assert np.true_divide([2],[8]) == 0.25
